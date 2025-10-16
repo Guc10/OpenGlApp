0c13348d2a4dbb13c2755526091f803eacd591e7
filipRadke <filip.radke@onet.pl>
2025-10-16 11:00:23 +0200

    Drawing triangle; Make it pretty

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -9,6 +9,7 @@ void main() {
     gl_Position = vec4(aPos, 0.0, 1.0);
 }
 )";
+
 const char* fragmentShaderSource = R"(
 #version 330 core
 out vec4 FragColor;
@@ -23,8 +24,10 @@ void processInput(GLFWwindow *window)
         glfwSetWindowShouldClose(window, true);
 }
 
+
 int main() {
     glfwInit();
+
     GLFWwindow* window = glfwCreateWindow(480, 480, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -32,6 +35,7 @@ int main() {
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -42,7 +46,7 @@ int main() {
     // Generate circle vertices
     const int num_segments = 20l;
     float vertices[2 * (num_segments + 2)];
-    float cx = 0.0f, cy = 0.0f, r = 1.0f;
+    float cx = 0.0f, cy = 0.0f, r = 0.4f;
     vertices[0] = cx;
     vertices[1] = cy;
     for (int i = 0; i <= num_segments; ++i) {
@@ -53,21 +57,50 @@ int main() {
 
     // VAO & VBO
     GLuint VAO, VBO;
+
     glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
     glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *) 0);
     glEnableVertexAttribArray(0);
+
+    // Triangle vertices
+
+    float triVertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    // Triangle VAO & VBO
+    GLuint triVAO, triVBO;
+
+    glGenVertexArrays(1, &triVAO);
+    glBindVertexArray(triVAO);
+
+    glGenBuffers(1, &triVBO);
+    glBindBuffer(GL_ARRAY_BUFFER,triVBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triVertices), triVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    glEnableVertexAttribArray(0);
+
+
 
     // Compile shaders
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
     glCompileShader(vertexShader);
+
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
     glCompileShader(fragmentShader);
+
+    // Link shaders
     GLuint shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
@@ -75,11 +108,19 @@ int main() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shaderProgram);
+
+        // Draw circle
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLE_FAN, 0, num_segments + 2);
+
+        // Draw triangle
+        glBindVertexArray(triVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         processInput(window);
 
