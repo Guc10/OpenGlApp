8e08afe60d4a5aca2c6cb043a48e455b8ab6efe1
Gustaw Grześkowiak <gustaw.grzeskowiak@gmail.com>
2025-12-03 13:15:15 +0100

    physics headers

--- app/src/physics/Ball.h
+++ app/src/physics/Ball.h
@@ -5,13 +5,36 @@
 #ifndef OPENGLAPP_BALL_H
 #define OPENGLAPP_BALL_H
 
+#include "glm/glm.hpp"
 
 class Ball {
-    public:
-        Ball(float reflectance);
-        ~Ball();
-        float Reflectance = 0;
-    private:
+public:
+    // Constructor
+    explicit Ball(const float reflectance, const float radius): Reflectance(reflectance), Radius(radius) {};
+    ~Ball();
+
+    // Update ball position based on movement vector and delta time or reset to start position
+    void UpdatePosition(glm::vec2 movementVector, float deltaTime);
+    void ResetPosition() { Position = StartPosition; };
+
+    // Setters
+    void SetReflectance(float reflectance) { Reflectance = reflectance; }
+    void SetRadius(float radius) { Radius = radius; }
+    void SetGravity(float gravity) { Gravity = gravity; }
+
+    // Getters
+    glm::vec2 GetPosition() const { return Position; }
+private:
+    // Ball properties
+    float Reflectance = 0;
+    float Radius = 0;
+    float Gravity = 9.81f;
+
+    glm::vec2 Velocity = glm::vec2(0.0f, 0.0f);
+
+    glm::vec2 Position = glm::vec2(0.0f, 0.0f);
+
+    const glm::vec2 StartPosition = glm::vec2(0.0f, 0.0f);
 };
 
 
--- app/src/physics/Movement.h
+++ app/src/physics/Movement.h
@@ -1,8 +1,12 @@
 #ifndef OPENGLAPP_MOVEMENT_H
 #define OPENGLAPP_MOVEMENT_H
 
+#include "glm/vec2.hpp"
 
 class Movement {
+public:
+    static glm::vec2 getMousePosition(float xpos, float ypos);
+    static glm::vec2 calculateVector(glm::vec2 currentBallPosition, glm::vec2 mousePosition);
 };
 
 
