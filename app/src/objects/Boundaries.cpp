9d0f20a619b55126f4bc34be1f20aafe2a2b046d
filipRadke <filip.radke@onet.pl>
2025-12-05 23:02:19 +0100

    Change the ball colision method

--- app/src/objects/Boundaries.cpp
+++ app/src/objects/Boundaries.cpp
@@ -26,34 +26,59 @@ Boundaries::~Boundaries() {
     }
 }
 
+glm::vec2 ClosestPointOnSegment(const glm::vec2& p, const glm::vec2& a, const glm::vec2& b)
+{
+    glm::vec2 ab = b - a;
+    float t = glm::dot(p - a, ab) / glm::dot(ab, ab);
+    t = glm::clamp(t, 0.0f, 1.0f);
+    return a + t * ab;
+}
+
 void Boundaries::ResolveCollision(Ball &ball) {
     glm::vec2 pos = ball.GetPosition();
     glm::vec2 vel = ball.GetVelocity();
     float r = ball.GetRadius();
     float refl = ball.GetReflectance();
 
-    // X axis collisions
-    if (pos.x - r < minX) {
-        pos.x = minX + r;
-        vel.x = -vel.x * refl;
-        // small damping on Y as well to simulate energy loss
-        vel.y *= refl;
-    } else if (pos.x + r > maxX) {
-        pos.x = maxX - r;
-        vel.x = -vel.x * refl;
-        vel.y *= refl;
-    }
+    // get vertices and indices
+    std::vector<glm::vec2> verts;
+    verts.reserve(trianglesCount);
 
-    // Y axis collisions
-    if (pos.y - r < minY) {
-        pos.y = minY + r;
-        vel.y = -vel.y * refl;
-        // horizontal damping on floor contact
-        vel.x *= refl;
-    } else if (pos.y + r > maxY) {
-        pos.y = maxY - r;
-        vel.y = -vel.y * refl;
-        vel.x *= refl;
+    for (int i = 0; i < trianglesCount * 3; i += 3)
+        verts.emplace_back(vertices[i], vertices[i+1]);
+
+    std::vector<unsigned int> idx(indices, indices + 24);
+
+    for (int i = 0; i < trianglesCount * 3; i += 3)
+    {
+        glm::vec2 a = verts[indices[i]];
+        glm::vec2 b = verts[indices[i+1]];
+        glm::vec2 c = verts[indices[i+2]];
+
+        // Check all 3 edges
+        glm::vec2 edgePts[3][2] = {{a,b}, {b,c}, {c,a}};
+
+        for (int e = 0; e < 3; e++)
+        {
+            glm::vec2 p1 = edgePts[e][0];
+            glm::vec2 p2 = edgePts[e][1];
+
+            glm::vec2 closest = ClosestPointOnSegment(pos, p1, p2);
+            glm::vec2 diff = pos - closest;
+            float dist = glm::length(diff);
+
+            if (dist < r)  // COLLISION
+            {
+                glm::vec2 normal = glm::normalize(diff);
+
+                // Push ball out of wall
+                pos = closest + normal * r;
+
+                // Reflect velocity
+                vel = vel - 2.0f * glm::dot(vel, normal) * normal;
+                vel *= refl; // damping
+            }
+        }
     }
 
     ball.SetPosition(pos);
